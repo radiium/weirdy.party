b8041ff934535e45aacb0438058b73a922821ebc
radiium <radiium@openmailbox.org>
2017-05-14 23:55:28 +0200

    Rework toolbar and navbar

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,16 +9,24 @@ function logout() {
 
 // Togle toolbar button
 $( "#toggle" ).click(function() {
+    // close
     if ($('#toolbar').css('width') === '185px') {
         $('#toggle').css('border-radius', '0');
         $('#toggle').toggleClass('rotation');
         $('#toolbar').css('width', '0');
         $('.toolbar-btn').css('width', '0');
+        if ($(window).width() < 425) {
+            $('#navbar').css('margin-left', '0');
+        }
+    // Open
     } else {
         $('#toggle').css('border-radius', '25px');
         $('#toggle').toggleClass('rotation');
         $('#toolbar').css('width', '185px');
         $('.toolbar-btn').css('width', '50px');
+        if ($(window).width() < 425) {
+            $('#navbar').css('margin-left', '55px');
+        }
     }
 });
 // Open close button from toolbar 
