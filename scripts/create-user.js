ddf7e72d6d946e0daaa703d253aab6f73bba3498
radiium <radiium@openmailbox.org>
2017-05-21 19:52:49 +0200

    Reorganize repo

--- scripts/create-user.js
+++
@@ -1,8 +0,0 @@
-db.createUser(
-{
-    user: "yeah",
-    pwd: "yeah",
-    roles: [
-      { role: "readWrite", db: "weirdydb" }
-    ]
-});
--- scripts/pass.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-var bcrypt   = require('bcrypt-nodejs');
-var test = bcrypt.hashSync(process.argv[2], bcrypt.genSaltSync(8), null);
-console.log(test)
