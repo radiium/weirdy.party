4554ff4189328b6f829cce94b6070a562f9bb893
radiium <radiium@openmailbox.org>
2017-05-21 20:04:42 +0200

    Clean gitignore an run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
 
-# Env
-ADMIN_USER=admin
-ADMIN_PASS=admin
-
-DB=weirdydb
-DB_USER=yeah
-DB_PASS=yeah
-
 # Build container
 # ttab  -a iTerm2 
 docker-compose down
 docker-compose build
 docker-compose up
-
-sleep 1
-
-docker exec db_mongodb mongo admin ./scripts/create-admin.js
-docker exec db_mongodb mongo admin ./scripts/create-user.js -u admin -p admin --authenticationDatabase admin
