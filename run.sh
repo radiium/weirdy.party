e46dec2b1b8921d3e492066fa7b91429ad2fafd7
radiium <radiium@openmailbox.org>
2017-05-23 23:57:11 +0200

    Update app dockerfile

--- run.sh
+++ run.sh
@@ -2,6 +2,31 @@
 
 # Build container
 # ttab  -a iTerm2 
+
+
+composeDev() {
+    docker-compose build
+    docker-compose up
+}
+
+composeProd() {
+    docker-compose build -f docker-compose-prod.yml
+    docker-compose up -f docker-compose-prod.yml
+}
+
+# Down container
 docker-compose down
-docker-compose build
-docker-compose up
+
+case "$1" in
+    dev)
+        composeDev
+        ;;
+    prod)
+        composeProd
+        ;;
+
+    *)
+    echo "Usage: $0 \n  app: Deploy APP\n   ds: Remove .DS_store}"
+    ;;
+esac
+echo "Ciao" >&2
